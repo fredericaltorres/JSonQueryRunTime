fc34319059e379fc60961fa97475e7bd77efe0e9
Frederic Torres <fredericaltorres@gmail.com>
2018-12-27 20:21:58 -0500

    Added performance unit tests

--- JSonQueryRunTime_UnitTests/JSonQueryRunTime_EvalJsonLines_UnitTests.cs
+++ JSonQueryRunTime_UnitTests/JSonQueryRunTime_EvalJsonLines_UnitTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace JSonQueryRunTime_UnitTests
 {
@@ -17,6 +18,7 @@ namespace JSonQueryRunTime_UnitTests
             l.Add(JSonQueryRunTime_EvalOneJsonString_UnitTests.json1);
             return l;
         }
+
 
         [TestMethod]
         public void Execute_String_Equal()
---
+++ JSonQueryRunTime_UnitTests/JSonQueryRunTime_Performance_UnitTests.cs
@@ -0,0 +1,39 @@
+﻿using HiSystems.Interpreter;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace JSonQueryRunTime_UnitTests
+{
+    [TestClass]
+    public class JSonQueryRunTime_Performance_UnitTests {
+    
+
+        public IEnumerable<string> GetJsonLines1()
+        {
+            var l = new List<string>();
+            for(var i=0; i< 50000; i++) {
+                l.Add(JSonQueryRunTime_EvalOneJsonString_UnitTests.json0);
+                l.Add(JSonQueryRunTime_EvalOneJsonString_UnitTests.json1);
+            }
+            return l;
+        }
+
+        [TestMethod]
+        public void Perf_Execute_String_Equal()
+        {
+            var lines = GetJsonLines1().ToList();
+
+            var sw = Stopwatch.StartNew();
+                var resultLines = new JsonQueryRuntime(@"name = ""ok"" ").Eval(lines).ToList();
+            sw.Stop();
+
+            var expectedCount = lines.Count/2;
+            Assert.AreEqual(expectedCount, resultLines.Count);
+            Assert.IsTrue(sw.Elapsed < new System.TimeSpan(0,0,3));            
+        }    
+
+        
+    }
+}
