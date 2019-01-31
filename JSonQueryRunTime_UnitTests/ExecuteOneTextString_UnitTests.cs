9dead5a12374f130fa7fa73ed223fdfdd8f17236
Frederic Torres <fredericaltorres@gmail.com>
2019-01-31 16:23:08 -0500

    Added more test cases to unit tests

--- JSonQueryRunTime_UnitTests/ExecuteOneTextString_UnitTests.cs
+++ JSonQueryRunTime_UnitTests/ExecuteOneTextString_UnitTests.cs
@@ -28,16 +28,26 @@ namespace JSonQueryRunTime_UnitTests
         {
             Assert.IsTrue(new JsonQueryRuntime(@" Regex(text, 'A..D') ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
             Assert.IsTrue(new JsonQueryRuntime(@" Regex(text, 'A..D') AND Regex(text, 'A..D') AND Regex(text, 'A..D') ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
+            Assert.IsFalse(new JsonQueryRuntime(@" Regex(text, 'A..E') ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
         }
         [TestMethod]
         public void WildCard()
         {
             Assert.IsTrue(new JsonQueryRuntime(@" Wildcard(text, 'A??D') ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
+            Assert.IsFalse(new JsonQueryRuntime(@" Wildcard(text, 'A??Z') ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
         }
         [TestMethod]
         public void InFunc()
         {
             Assert.IsTrue(new JsonQueryRuntime(@" In(text, Array('ABCD', 'XYZ')) ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
+            Assert.IsFalse(new JsonQueryRuntime(@" In(text, Array('OOO', 'XYZ')) ").Execute(sourceTextABCD, JsonQueryRuntimeTextType.TEXT));
+        }
+        [TestMethod]
+        public void Range()
+        {
+            var source = "B";
+            Assert.IsTrue(new JsonQueryRuntime(@" Range(text, 'A', 'Z') ").Execute(source, JsonQueryRuntimeTextType.TEXT));
+            Assert.IsFalse(new JsonQueryRuntime(@" Range(text, 'BB', 'Z') ").Execute(source, JsonQueryRuntimeTextType.TEXT));
         }
     }
 }
